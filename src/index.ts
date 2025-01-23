03e6a7188ae2ba5b1e2ec458bc9d13d5ec766f00
RainJonathan <reinjonathannn@gmail.com>
2025-01-23 16:18:26 +0700

    feat: Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,43 @@
-import { AppDataSource } from "./data-source";
-import * as express from "express";
-import * as dotenv from "dotenv";
-import { Request, Response } from "express";
-import * as morgan from "morgan";
-import "reflect-metadata";
-import { userRouter } from "./routes/user.routes";
-import { movieRouter } from "./routes/movie.routes";
-import { carRouter } from "./routes/car.routes";
-import { orderRouter } from "./routes/order.routes";
-import { errorHandler } from "./middleware/error.middleware";
-import { notificationRouter } from "./routes/notification.routes";
-dotenv.config();
+  import { AppDataSource } from "./data-source";
+  import * as express from "express";
+  import * as dotenv from "dotenv";
+  import { Request, Response } from "express";
+  import * as morgan from "morgan";
+  import "reflect-metadata";
+  import { userRouter } from "./routes/user.routes";
+  import { movieRouter } from "./routes/movie.routes";
+  import { carRouter } from "./routes/car.routes";
+  import { orderRouter } from "./routes/order.routes";
+  import { errorHandler } from "./middleware/error.middleware";
+  import { notificationRouter } from "./routes/notification.routes";
+  dotenv.config();
 
-const app = express();
+  const app = express();
 
-// Use morgan for logging
-app.use(morgan("combined"));
+  // Use morgan for logging
+  app.use(morgan("combined"));
+  app.use(cors());
 
-app.use(express.json());
-app.use(errorHandler);
+  app.use(express.json());
+  app.use(errorHandler);
 
-const { PORT = 3000 } = process.env;
+  const { PORT = 3000 } = process.env;
 
-app.use("/auth", userRouter);
-app.use("/api", movieRouter);
-app.use("/car", carRouter);
-app.use("/order", orderRouter);
-app.use("/notification", notificationRouter);
+  app.use("/auth", userRouter);
+  app.use("/api", movieRouter);
+  app.use("/car", carRouter);
+  app.use("/order", orderRouter);
+  app.use("/notification", notificationRouter);
 
-app.get("*", (req: Request, res: Response) => {
-  res.status(505).json({ message: "Bad Request" });
-});
+  app.get("*", (req: Request, res: Response) => {
+    res.status(505).json({ message: "Bad Request" });
+  });
 
-AppDataSource.initialize()
-  .then(async () => {
-    app.listen(PORT, () => {
-      console.log("Server is running on http://localhost:" + PORT);
-    });
-    console.log("Data Source has been initialized!");
-  })
-  .catch((error) => console.log(error));
+  AppDataSource.initialize()
+    .then(async () => {
+      app.listen(PORT, () => {
+        console.log("Server is running on http://localhost:" + PORT);
+      });
+      console.log("Data Source has been initialized!");
+    })
+    .catch((error) => console.log(error));
